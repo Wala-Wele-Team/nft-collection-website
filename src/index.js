1e2e6e34ea9e5d9b14bb43010f3a1f142fa3d082
altrawan <muhammadalifputra8888@gmail.com>
2022-03-13 21:03:12 +0700

    Add About Section

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import './index.css';
 import App from "./App";
 import "../node_modules/normalize.css/normalize.css";
 import { BrowserRouter } from "react-router-dom";
 // This will remove default browser css
-// import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -16,7 +14,3 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
