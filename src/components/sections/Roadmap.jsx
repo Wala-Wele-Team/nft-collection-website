8ced34a24c67d396a7a3f85b532a8cd9e1cc84d4
sereinisme <rizkiselipratama@gmail.com>
2022-03-16 20:29:20 +0700

    roadmap

--- src/components/sections/Roadmap.jsx
+++ src/components/sections/Roadmap.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Section = styled.section`
+min-height: 100vh;
+width: 100vw;
+background-color: ${props => props.theme.body};
+position: relative;
+`
+
 
 const Roadmap = () => {
   return (
-    <div>Roadmap</div>
+    <Section>Roadmap</Section>
   )
 }
 
