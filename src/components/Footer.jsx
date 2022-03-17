f947b929549f6d754dbbafd36900092cd67e396e
altrawan <muhammadalifputra8888@gmail.com>
2022-03-17 09:24:00 +0700

    Create Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-<<<<<<< HEAD
     <Section>Footer</Section>
-=======
-    <div>Footer1</div>
->>>>>>> c02ccb86f9ee3912cec31e02453d43905776ca5a
   )
 }
 
